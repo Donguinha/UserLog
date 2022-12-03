f6285c038951839127cce277e654dca3f50fa299
Guilherme Rodrigues <oliveirack1@gmail.com>
2022-12-03 14:13:28 -0300

    Users acess

---
+++ LogUser/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace LogUser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<Users> userlog = LerArquivo();
+
+            foreach (Users each in userlog)
+            {
+                Console.WriteLine(each);
+            }
+
+            Console.WriteLine("users number: " + userlog.Count); 
+        }
+
+        static SortedSet<Users> LerArquivo()
+        {
+            SortedSet<Users> user1 = new SortedSet<Users>();
+            try
+            {
+                string path = @"c:\code\LogUser\in.txt";
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!(sr.EndOfStream))
+                    {
+                        string[] str1 = sr.ReadLine().Split(' ');
+                        DateTime date = DateTime.Parse(str1[1]);
+
+                        user1.Add(new Users(str1[0], date));
+                    }
+                }
+                return user1;
+            }
+            catch (IOException excpetion)
+            {
+                Console.WriteLine("Reading error");
+                return null;
+            }
+        }
+    }
+}
---
+++ LogUser/Users.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LogUser
+{
+    public class Users : IComparable
+    {
+        public string username { get; set; }
+        public DateTime dateacess { get; set; }
+
+        public Users(string username, DateTime dateacess)
+        {
+            this.username = username;
+            this.dateacess = dateacess;
+        }
+
+        public override int GetHashCode()
+        {
+            return username.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Users))
+            {
+                return false;
+            }
+
+            Users user = (Users)obj;
+
+            return username.Equals(user.username);
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (!(obj is Users))
+            {
+                throw new ArgumentException("Error: is not a User");
+            }
+            Users user10 = (Users)obj;
+            return username.CompareTo(user10.username);
+        }
+
+        public override string ToString()
+        {
+            return $"{username} {dateacess}";
+        }
+    }
+}
